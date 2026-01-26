06f78db7e174ea8c04ebe9d51e3e034018be62f2
RohanPoloju899 <polojurohan1@gmail.com>
2026-01-26 18:58:29 +0530

    Update higher_order_function2.js

--- 26_01/higher_order_function2.js
+++ 26_01/higher_order_function2.js
@@ -9,3 +9,20 @@ function fun1(){
 
 fun("Rohan",fun1);
 
+const variable=[1,2,3,4,5];
+
+const variable1=variable.map(function(variable){
+    return variable ** 2;
+});
+console.log(variable1);
+
+const variable2=variable.filter(function(variable){
+    return variable%2===0;
+});
+console.log(variable2);
+
+const variable3=variable.reduce(function(variable,variable1){
+    return variable+variable1;
+});
+console.log(variable3);
+
